3f8d7ab2327f53c9d637a8e56d483fe33c4b9ccb
Thiago Ferreira <thi.ferreira.silva03@gmail.com>
2022-07-17 11:26:07 -0300

    Security configs

--- backend/src/main/java/com/devsuperior/dsmetac/DsmetaCApplication.java
+++ backend/src/main/java/com/devsuperior/dsmetac/DsmetaCApplication.java
@@ -1,13 +1,13 @@
-package com.devsuperior.dsmeta;
+package com.devsuperior.dsmetac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsmetaApplication {
+public class DsmetaCApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DsmetaApplication.class, args);
+		SpringApplication.run(DsmetaCApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/devsuperior/dsmetac/DsmetaCApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dsmetac/DsmetaCApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devsuperior.dsmeta;
+package com.devsuperior.dsmetac;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DsmetaApplicationTests {
+class DsmetaCApplicationTests {
 
 	@Test
 	void contextLoads() {
