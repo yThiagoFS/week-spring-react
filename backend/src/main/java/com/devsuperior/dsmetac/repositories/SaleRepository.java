fb797472ede2763a6ebbc2bc34670203fade796a
Thiago Ferreira <thi.ferreira.silva03@gmail.com>
2022-07-17 12:27:09 -0300

    Day Select

--- backend/src/main/java/com/devsuperior/dsmetac/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmetac/repositories/SaleRepository.java
@@ -2,12 +2,18 @@ package com.devsuperior.dsmetac.repositories;
 
 
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmetac.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale,Long>{
 
 	
-	
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
